326b1e913a74eb7ac3cfc37a68c79cfc6c59fc87
Chethram Ramoutar <cramoutar1337@gmail.com>
2022-11-20 18:35:58 -0500

    Added SQL Files

---
+++ FGO/SQL Table Files/Creation Script.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE FGO;
+SET SQL_SAFE_UPDATES = 0;
+CREATE TABLE Servants(
+	ServantNumber INT NOT NULL AUTO_INCREMENT,
+	ServantName VARCHAR(500) NOT NULL,
+    StarRarity INT NOT NULL,
+    isOwned BOOLEAN NOT NULL,
+    PRIMARY KEY (ServantNumber)
+);
+
+ CREATE TABLE NoblePhantasms(
+	NpID INT NOT NULL AUTO_INCREMENT,
+	NpName VARCHAR (500) NOT NULL,
+    NpLevel INT NOT NULL,
+    CorrespondingServantNumber INT NOT NULL,
+    PRIMARY KEY (NpID)
+);
+
+update noblephantasms
+set CorrespondingServantNumber = NpID
+where CorrespondingServantNumber = 0;
+
+ALTER TABLE servants ADD UNIQUE(ServantName);
+ALTER TABLE noblephantasms ADD FOREIGN KEY (CorrespondingServantNumber) REFERENCES servants(servantnumber);
+SET SQL_SAFE_UPDATES = 1;
+
+#select servants.starrarity, servants.Servantname, noblephantasms.Npname, noblephantasms.nplevel, servants.isowned
+#	FROM servants
+#	 JOIN Noblephantasms ON servants.servantnumber=noblephantasms.npid
+#		order by servants.servantnumber;
